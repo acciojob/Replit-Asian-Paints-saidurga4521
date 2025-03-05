baac3685b84b82e4f19574fc77df17e1f408096f
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-05 16:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const blockId=document.getElementById('block-id');
+const blockId=document.getElementById('block_id');
 const color=document.getElementById('colour-id');
 const changeColor=document.getElementById('change-button');
-const reset=document.getElementById('Reset');
+const reset=document.getElementById('reset_button');
 changeColor.addEventListener('click',()=>{
 	var gridItem=document.querySelectorAll('.grid-item');
 	gridItem.forEach((item)=>{
